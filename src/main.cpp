b8638107bfc43663ab28faf0d1cc5f35a6c6c9f2
TAKAHARA <mwl9dv@gmail.com>
2020-10-13 09:13:37 +0900

    ローカルでの計測を完了

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,59 @@
 #include <Arduino.h>
 #include <MySGP30.h>
+#include <math.h>
 
 MySGP30 sgp;
+
+void measure();
+void calc();
+
+const int N = 10;
+int countBreathing = 0, countCanCalc = 0;
+double vals[N], rate, co2max, co2min, co2prod = 0;
+bool isReached = false;
+// 1分間の呼吸回数
+// 各呼吸における二酸化炭素排出量を1分間記録
+
+void calc() {
+    const int HR = 60, VO2MAX = 60, AGE = 20, W = 70;
+    const int HRR = 220 - AGE - HR;
+    int hr = 4 * countBreathing;
+    double pHRR = (double)hr / (double)HRR;
+    Serial.println("%HRR: " + String(pHRR));
+    double vo2 = VO2MAX * pHRR;
+    double e = 3.9 * vo2 / W + 1.1 * co2prod * 0.001 / W;
+    co2prod = 0;
+    Serial.println("エネルギー: " + String(e));
+}
+
+void measure() {
+    rate = ((vals[0] - vals[9]) / vals[9]) * 100;
+    if (rate > 30 && !isReached) {
+        Serial.println("TOP");
+        co2max = vals[9];
+        isReached = true;
+        countCanCalc++;
+    } else if (rate < -30 && isReached) {
+        Serial.println("BTM");
+        co2min = vals[9];
+        isReached = false;
+        countCanCalc++;
+    }
+    if (countCanCalc == 2) {
+        co2prod += fabs(co2max - co2min);
+        countBreathing++;
+        countCanCalc = 0;
+        Serial.println("CO2生成量: " + String(co2prod));
+    }
+
+    for (int i = N - 1; i > 0; i--) {
+        vals[i] = vals[i - 1];
+    }
+    vals[0] = sgp.getECO2();
+}
+
+unsigned long lastResetingTime = 0;
+const unsigned long resetingInterval = 60 * 1000L; // 60 sec
 
 void setup() {
     Serial.begin(115200);
@@ -18,8 +70,6 @@ void setup() {
       sensor!
       */
 }
-
-int counter = 0;
 
 void loop() {
 
@@ -38,22 +88,22 @@ void loop() {
         return;
     }
 
+    if (millis() - lastResetingTime > resetingInterval) {
+        Serial.println("************************************");
+        lastResetingTime = millis();
+        Serial.println(co2prod);
+        Serial.println(countBreathing);
+        calc();
+        co2prod = 0;
+        countBreathing = 0;
+        Serial.println("************************************");
+    }
+
     Serial.print("eCO2 ");
     Serial.print(sgp.getECO2());
     Serial.println(" ppm");
 
+    measure();
+
     delay(100);
-
-    counter++;
-    if (counter == 300) {
-        counter = 0;
-
-        if (!sgp.canGetIAQBaseLine()) {
-            Serial.println("Failed to get baseline readings");
-            return;
-        }
-        Serial.print("****Baseline: ");
-        Serial.println("eCO2: 0x" + String(sgp.getECO2Base(), HEX));
-        Serial.println(" & TVOC: 0x" + String(sgp.getTVOCBase(), HEX));
-    }
 }
